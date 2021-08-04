79d14945d5f0460ce95140d5224b7b8f5255b5a4
brainfucksec <brainfucksec@protonmail.com>
2021-08-04 11:40:32 +0200

    Update keymapping

--- nvim/lua/keymapping.lua
+++ nvim/lua/keymapping.lua
@@ -7,14 +7,16 @@
 
 local map = vim.api.nvim_set_keymap
 
+local defaults = {noremap = true, silent = true}
+
 -- basic autopair
-map('i', '"', '""<left>', {noremap = true, silent = true})
-map('i', '`', '``<left>', {noremap = true, silent = true})
-map('i', '(', '()<left>', {noremap = true, silent = true})
-map('i', '[', '[]<left>', {noremap = true, silent = true})
-map('i', '{', '{}<left>', {noremap = true, silent = true})
-map('i', '{<CR>', '{<CR}<ESC>0', {noremap = true, silent = true})
-map('i', '{;<CR>', '{<CR};<ESC>0', {noremap = true, silent = true})
+map('i', '"', '""<left>', defaults)
+map('i', '`', '``<left>', defaults)
+map('i', '(', '()<left>', defaults)
+map('i', '[', '[]<left>', defaults)
+map('i', '{', '{}<left>', defaults)
+map('i', '{<CR>', '{<CR}<ESC>0', defaults)
+map('i', '{;<CR>', '{<CR};<ESC>0', defaults)
 
 -- autocomplete with Tab
 map('i', '<Tab>', 'v:lua.tab_complete()', {expr = true})
@@ -23,7 +25,7 @@ map('i', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
 map('s', '<S-Tab>', 'v:lua.s_tab_complete()', {expr = true})
 
 -- clear search highlighting
-map('n', '<leader>c', ':nohl<CR>', {noremap = true, silent = true})
+map('n', '<leader>c', ':nohl<CR>', defaults)
 
 -- press kk to exit
 map('i', 'kk', '<Esc>', {noremap = true})
@@ -35,19 +37,19 @@ map('', '<left>', '<nop>', {noremap = true})
 map('', '<right>', '<nop>', {noremap = true})
 
 -- fast saving
-map('n', '<leader>s', ':w<cr>', {noremap = true, silent = true})
-map('i', '<leader>s', '<C-c>:w<cr>', {noremap = true, silent = true})
+map('n', '<leader>s', ':w<cr>', defaults)
+map('i', '<leader>s', '<C-c>:w<cr>', defaults)
 
 -- move around splits using Ctrl + {h,j,k,l}
-map('n', '<C-h>', '<C-w>h', {noremap = true, silent = true})
-map('n', '<C-j>', '<C-w>j', {noremap = true, silent = true})
-map('n', '<C-k>', '<C-w>k', {noremap = true, silent = true})
-map('n', '<C-l>', '<C-w>l', {noremap = true, silent = true})
+map('n', '<C-h>', '<C-w>h', defaults)
+map('n', '<C-j>', '<C-w>j', defaults)
+map('n', '<C-k>', '<C-w>k', defaults)
+map('n', '<C-l>', '<C-w>l', defaults)
 
 -- nvim-tree shortcuts
-map('n', '<C-n>', ':NvimTreeToggle<CR>', {noremap = true, silent = true})
-map('n', '<leader>r', ':NvimTreeRefresh<CR>', {noremap = true, silent = true})
-map('n', '<leader>n', ':NvimTreeFindFile<CR>', {noremap = true, silent = true})
+map('n', '<C-n>', ':NvimTreeToggle<CR>', defaults)
+map('n', '<leader>r', ':NvimTreeRefresh<CR>', defaults)
+map('n', '<leader>n', ':NvimTreeFindFile<CR>', defaults)
 
 -- open/close tagbar
-map('', '<C-m>', ':TagbarToggle<CR>', {noremap = true, silent = true})
+map('', '<C-m>', ':TagbarToggle<CR>', defaults)
