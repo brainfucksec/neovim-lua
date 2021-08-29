92b43579580a80e9f36f37a6483d078c6c329633
brainfucksec <brainfucksec@protonmail.com>
2021-08-29 21:47:11 +0200

    Update /lua/keymaps.lua

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -30,15 +30,18 @@ map('', '<down>', '<nop>', {noremap = true})
 map('', '<left>', '<nop>', {noremap = true})
 map('', '<right>', '<nop>', {noremap = true})
 
--- fast saving
-map('n', '<leader>s', ':w<cr>', default_opts)
-map('i', '<leader>s', '<C-c>:w<cr>', default_opts)
+-- fast saving with <leader> and s
+map('n', '<leader>s', ':w<CR>', default_opts)
+map('i', '<leader>s', '<C-c>:w<CR>', default_opts)
 
 -- move around splits using Ctrl + {h,j,k,l}
 map('n', '<C-h>', '<C-w>h', default_opts)
 map('n', '<C-j>', '<C-w>j', default_opts)
 map('n', '<C-k>', '<C-w>k', default_opts)
 map('n', '<C-l>', '<C-w>l', default_opts)
+
+-- close all windows and exit from neovim
+map('n', '<leader>q', ':qa<CR>', default_opts)
 
 -----------------------------------------------------------
 -- Plugins shortcuts:
