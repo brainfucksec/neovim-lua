98b0f00ed7b355e6770eb4a0c238a53ba03f7fe9
brainfucksec <brainfucksec@protonmail.com>
2021-09-15 08:42:28 +0200

    Fix (2) nvim/lua/plugins/paq-nvim.lua

--- nvim/lua/plugins/paq-nvim.lua
+++ nvim/lua/plugins/paq-nvim.lua
@@ -21,7 +21,7 @@ require 'paq' {
   'neovim/nvim-lspconfig';
   'hrsh7th/nvim-cmp';
   'hrsh7th/cmp-nvim-lsp';
-  'hrsh7th /cmp-path';
+  'hrsh7th/cmp-path';
   'hrsh7th/cmp-buffer';
   'tanvirtin/monokai.nvim';
 }
