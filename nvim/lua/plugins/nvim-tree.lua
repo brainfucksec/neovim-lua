33c2d42f0bb05afbb1b6ad1d18de7129d4f422ce
brainfucksec <brainfucksec@protonmail.com>
2021-09-26 07:30:45 +0200

    v0.1.0 - 2021-09-26, Upgrade: switch from alpha versioning

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -6,17 +6,30 @@
 -- https://github.com/kyazdani42/nvim-tree.lua
 
 
+--[[
+
+Keybindings are defined in `keymapping.lua`:
+https://github.com/kyazdani42/nvim-tree.lua#keybindings
+
+Note: options under the g: command should be set BEFORE running the
+setup function:
+https://github.com/kyazdani42/nvim-tree.lua#setup
+
+See: `help NvimTree`
+
+]]--
+
+
 local g = vim.g
 
-g.nvim_tree_width = 30
+--g.nvim_tree_width = 30
 g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
 g.nvim_tree_gitignore = 1
-g.nvim_tree_auto_open = 1
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
+g.nvim_tree_highlight_opened_files = 1
 g.nvim_tree_width_allow_resize  = 1
-g.nvim_tree_special_files = {'README.md', 'Makefile', 'MAKEFILE'}
 g.nvim_tree_show_icons = {
   git = 1,
   folders = 1,
@@ -27,7 +40,7 @@ g.nvim_tree_icons = {
 	default = "‣ "
 }
 
--- Keybindings are defined in `keymapping.lua`
---- See: `help NvimTree`
---- https://github.com/kyazdani42/nvim-tree.lua#keybindings
-
+require('nvim-tree').setup{
+  -- open nvim-tree at startup if no file is specified
+  open_on_setup = true
+}
