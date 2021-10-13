99cb6d7848c8e9a46112d6fef56e1e9cb2698a06
brainfucksec <brainfucksec@protonmail.com>
2021-10-13 22:21:49 +0200

    Update /plugins/nvim-tree.lua

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -22,12 +22,15 @@ See: `help NvimTree`
 
 local g = vim.g
 
-g.nvim_tree_ignore = {'.git', 'node_modules', '.cache'}
+g.nvim_tree_ignore = {'.git', 'node_modules', '.cache', '.bin'}
 g.nvim_tree_gitignore = 1
+g.nvim_tree_quit_on_open = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_hide_dotfiles = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
+g.nvim_tree_disable_window_picker = 1
+g.nvim_tree_respect_buf_cwd = 1
 g.nvim_tree_width_allow_resize  = 1
 g.nvim_tree_show_icons = {
   git = 1,
@@ -40,6 +43,7 @@ g.nvim_tree_icons = {
 }
 
 require('nvim-tree').setup{
-  -- open nvim-tree at startup if no file is specified
+  -- open the tree when running this setup function
   open_on_setup = true,
+  view = { auto_resize = true },
 }
