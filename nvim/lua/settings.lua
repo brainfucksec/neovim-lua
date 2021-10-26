92b026a03e2732a793c94197ea2091c1b5d55af1
brainfucksec <brainfucksec@protonmail.com>
2021-10-26 10:29:50 +0200

    Update /settings.lua

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -131,5 +131,8 @@ local disabled_built_ins = {
 }
 
 for _, plugin in pairs(disabled_built_ins) do
-    vim.g["loaded_" .. plugin] = 1
+    g["loaded_" .. plugin] = 1
 end
+
+-- disable nvim intro
+opt.shortmess:append "sI"
