8e90acfd5ca65a7705bc879b3edb6bce9dccadc8
brainfucksec <brainfucksec@protonmail.com>
2022-02-13 09:50:36 +0100

    Update /plugins/alpha-nvim.lua

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -5,18 +5,23 @@
 -- Plugin: alpha-nvim
 -- url: https://github.com/goolord/alpha-nvim
 
+-- For configuration examples see: https://github.com/goolord/alpha-nvim/discussions/16
+
 
 local alpha = require 'alpha'
 local dashboard = require 'alpha.themes.dashboard'
 
--- Setup footer
+-- Footer
 local function footer()
+  local version = vim.version()
+  local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
   local datetime = os.date('%Y/%m/%d %H:%M:%S')
-  return datetime
+
+  return print_version .. ' ' .. datetime
 end
 
--- Header
-dashboard.section.header.val = {
+-- Banner
+local banner = {
   "                                                    ",
   " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
   " ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
@@ -27,9 +32,11 @@ dashboard.section.header.val = {
   "                                                    ",
 }
 
+dashboard.section.header.val = banner
+
 -- Menu
 dashboard.section.buttons.val = {
-  dashboard.button('e', ' New file', ':ene <BAR> startinsert<CR>'),
+  dashboard.button('e', ' New file', ':ene <BAR> startinsert<CR>'),
   dashboard.button('f', ' Find file', ':NvimTreeOpen<CR>'),
   dashboard.button('s', ' Settings', ':e $MYVIMRC<CR>'),
   dashboard.button('u', ' Update plugins', ':PackerUpdate<CR>'),
