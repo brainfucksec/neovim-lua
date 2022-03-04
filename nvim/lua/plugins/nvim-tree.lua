d0bb519fe98602f9ff319b1ba68c685271223af1
brainfucksec <brainfucksec@protonmail.com>
2022-03-04 07:20:10 +0100

    Update /lua/plugins/nvim-tree.lua

--- nvim/lua/plugins/nvim-tree.lua
+++ nvim/lua/plugins/nvim-tree.lua
@@ -5,19 +5,18 @@
 -- Plugin: nvim-tree
 -- url: https://github.com/kyazdani42/nvim-tree.lua
 
---- Keybindings are defined in `keymapping.lua`:
+--- Keybindings are defined in `core/keymapping.lua`:
 --- https://github.com/kyazdani42/nvim-tree.lua#keybindings
 
 --- Note: options under the g: command should be set BEFORE running the
 --- setup function: https://github.com/kyazdani42/nvim-tree.lua#setup
+--- Default options are not included.
 --- See: `help NvimTree`
 local g = vim.g
 
-g.nvim_tree_quit_on_open = 0
 g.nvim_tree_indent_markers = 1
 g.nvim_tree_git_hl = 1
 g.nvim_tree_highlight_opened_files = 1
-g.nvim_tree_disable_window_picker = 1
 g.nvim_tree_respect_buf_cwd = 1
 g.nvim_tree_width_allow_resize  = 1
 g.nvim_tree_show_icons = {
@@ -32,13 +31,22 @@ g.nvim_tree_icons = {
 
 require('nvim-tree').setup {
   open_on_setup = true,
+  auto_close = false,
   update_cwd = true,
-  view = {
-    width = 32,
-    auto_resize = true,
+  actions = {
+    open_file = {
+      resize_window = false,
+      quit_on_open = false,
+      window_picker = { enable = false },
+    },
+  },
+  system_open = {
+    cmd  = nil,
+    args = {}
   },
   filters = {
     dotfiles = true,
     custom = { '.git', 'node_modules', '.cache', '.bin' },
   },
+  view = { width = 32 },
 }
