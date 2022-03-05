d5b0b30798822a3b3f530b8fa9ba725b1468ca9f
brainfucksec <brainfucksec@protonmail.com>
2022-03-05 08:03:31 +0100

    Update settings (TODO: improve Autocmd)

--- nvim/lua/core/settings.lua
+++ nvim/lua/core/settings.lua
@@ -2,34 +2,65 @@
 -- General Neovim settings and configuration
 -----------------------------------------------------------
 
+-- Default options are not included
+--- See: https://neovim.io/doc/user/vim_diff.html
+--- [2] Defaults - *nvim-defaults*
+
 -----------------------------------------------------------
 -- Neovim API aliases
 -----------------------------------------------------------
---local fn = vim.fn       				-- Call Vim functions
-local cmd = vim.cmd     				-- Execute Vim commands
-local exec = vim.api.nvim_exec 	-- Execute Vimscript
-local g = vim.g         				-- Global variables
-local opt = vim.opt         		-- Set options (global/buffer/windows-scoped)
+local cmd = vim.cmd     				      -- Execute Vim commands
+local exec = vim.api.nvim_exec 	      -- Execute Vimscript
+local g = vim.g         				      -- Global variables
+local opt = vim.opt         		      -- Set options (global/buffer/windows-scoped)
+--local fn = vim.fn       				    -- Call Vim functions
 
 -----------------------------------------------------------
 -- General
 -----------------------------------------------------------
-opt.mouse = 'a'                 -- Enable mouse support
-opt.clipboard = 'unnamedplus'   -- Copy/paste to system clipboard
-opt.swapfile = false            -- Don't use swapfile
+opt.mouse = 'a'                       -- Enable mouse support
+opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
+opt.swapfile = false                  -- Don't use swapfile
+opt.completeopt = 'menuone,noselect'  -- Autocomplete options
 
 -----------------------------------------------------------
 -- Neovim UI
 -----------------------------------------------------------
-opt.number = true               -- Show line number
-opt.showmatch = true            -- Highlight matching parenthesis
-opt.foldmethod = 'marker'       -- Enable folding (default 'foldmarker')
-opt.colorcolumn = '80'          -- Line lenght marker at 80 columns
-opt.splitright = true           -- Vertical split to the right
-opt.splitbelow = true           -- Orizontal split to the bottom
-opt.ignorecase = true           -- Ignore case letters when search
-opt.smartcase = true            -- Ignore lowercase for the whole pattern
-opt.linebreak = true            -- Wrap on word boundary
+opt.number = true                     -- Show line number
+opt.showmatch = true                  -- Highlight matching parenthesis
+opt.foldmethod = 'marker'             -- Enable folding (default 'foldmarker')
+opt.colorcolumn = '80'                -- Line lenght marker at 80 columns
+opt.splitright = true                 -- Vertical split to the right
+opt.splitbelow = true                 -- Orizontal split to the bottom
+opt.ignorecase = true                 -- Ignore case letters when search
+opt.smartcase = true                  -- Ignore lowercase for the whole pattern
+opt.linebreak = true                  -- Wrap on word boundary
+
+-----------------------------------------------------------
+-- Tabs, indent
+-----------------------------------------------------------
+opt.expandtab = true                  -- Use spaces instead of tabs
+opt.shiftwidth = 4                    -- Shift 4 spaces when tab
+opt.tabstop = 4                       -- 1 tab == 4 spaces
+opt.smartindent = true                -- Autoindent new lines
+
+-----------------------------------------------------------
+-- Memory, CPU
+-----------------------------------------------------------
+opt.hidden = true                     -- Enable background buffers
+opt.history = 100                     -- Remember N lines in history
+opt.lazyredraw = true                 -- Faster scrolling
+opt.synmaxcol = 240                   -- Max column for syntax highlight
+
+-----------------------------------------------------------
+-- Colorscheme
+-----------------------------------------------------------
+opt.termguicolors = true              -- Enable 24-bit RGB colors
+cmd [[colorscheme monokai]]
+
+-----------------------------------------------------------
+-- Autocommands
+-----------------------------------------------------------
 
 -- Remove whitespace on save
 cmd [[au BufWritePre * :%s/\s\+$//e]]
@@ -42,28 +73,6 @@ exec([[
   augroup end
 ]], false)
 
------------------------------------------------------------
--- Memory, CPU
------------------------------------------------------------
-opt.hidden = true               -- Enable background buffers
-opt.history = 100               -- Remember N lines in history
-opt.lazyredraw = true           -- Faster scrolling
-opt.synmaxcol = 240             -- Max column for syntax highlight
-
------------------------------------------------------------
--- Colorscheme
------------------------------------------------------------
-opt.termguicolors = true        -- Enable 24-bit RGB colors
-cmd [[colorscheme monokai]]
-
------------------------------------------------------------
--- Tabs, indent
------------------------------------------------------------
-opt.expandtab = true            -- Use spaces instead of tabs
-opt.shiftwidth = 4              -- Shift 4 spaces when tab
-opt.tabstop = 4                 -- 1 tab == 4 spaces
-opt.smartindent = true          -- Autoindent new lines
-
 -- Don't auto commenting new lines
 cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
 
@@ -74,13 +83,6 @@ cmd [[autocmd FileType text,markdown,html,xhtml,javascript setlocal cc=0]]
 cmd [[
   autocmd FileType xml,html,xhtml,css,scss,javascript,lua,yaml setlocal shiftwidth=2 tabstop=2
 ]]
-
------------------------------------------------------------
--- Autocompletion
------------------------------------------------------------
-
--- Insert mode completion options
-opt.completeopt = 'menuone,noselect'
 
 -----------------------------------------------------------
 -- Terminal
