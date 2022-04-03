11118f8d9ef21ac946e5db32817911765a537cfd
brainfucksec <brainfucksec@protonmail.com>
2022-04-03 19:41:50 +0200

    Update nvim/lua/plugins/alpha-nvim.lua

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -36,11 +36,11 @@ dashboard.section.header.val = banner
 
 -- Menu
 dashboard.section.buttons.val = {
-  dashboard.button('e', ' New file', ':ene <BAR> startinsert<CR>'),
-  dashboard.button('f', ' Find file', ':NvimTreeOpen<CR>'),
-  dashboard.button('s', ' Settings', ':e $MYVIMRC<CR>'),
-  dashboard.button('u', ' Update plugins', ':PackerUpdate<CR>'),
-  dashboard.button('q', ' Quit', ':qa<CR>'),
+  dashboard.button('e', '  New file', ':ene <BAR> startinsert<CR>'),
+  dashboard.button('f', '  Find file', ':NvimTreeOpen<CR>'),
+  dashboard.button('s', '  Settings', ':e $MYVIMRC<CR>'),
+  dashboard.button('u', '  Update plugins', ':PackerUpdate<CR>'),
+  dashboard.button('q', '  Quit', ':qa<CR>'),
 }
 
 dashboard.section.footer.val = footer()
