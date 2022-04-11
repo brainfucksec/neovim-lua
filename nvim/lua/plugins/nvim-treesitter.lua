816fa78e95386e216062234b82a4d7db776ef53f
brainfucksec <brainfucksec@protonmail.com>
2022-04-11 15:12:35 +0200

    v0.57.0 - Update treesitter

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -11,8 +11,17 @@ if not status_ok then
   return
 end
 
+-- See: https://github.com/nvim-treesitter/nvim-treesitter#quickstart
 nvim_treesitter.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = {
+    'bash', 'c', 'cpp', 'html', 'javascript', 'json', 'lua', 'python',
+    'typescript', 'vim'
+  },
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
   highlight = {
+    -- `false` will disable the whole extension
     enable = true,
   },
 }
