9b5c28e0c57b2ead9361dddca2e4f3a8f6e12047
brainfucksec <brainfucksec@protonmail.com>
2022-04-16 09:20:42 +0200

    Fix declaration for Vista: nvim/lua/plugins/vista.lua, see: commit

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,7 @@
 --[[
 
 Neovim init file
-Version: 0.56.0 - 2022/04/12
+Version: 0.56.1 - 2022/04/16
 Maintainer: brainf+ck
 Website: https://github.com/brainfucksec/neovim-lua
 
--- nvim/lua/plugins/vista.lua
+++ nvim/lua/plugins/vista.lua
@@ -15,7 +15,7 @@ end
 -- This could make the display more compact or more spacious.
 -- e.g., more compact: ["▸ ", ""]
 -- Note: this option only works for the kind renderer, not the tree renderer
-vim.g.vista_icon_indent = '["╰─▸ ", "├─▸ "]'
+vim.g.vista_icon_indent = {'╰─▸ ', '├─▸ '}
 
 -- Executive used when opening vista sidebar without specifying it.
 -- See all the avaliable executives via `:echo g:vista#executives`.
@@ -23,16 +23,5 @@ vim.g.vista_default_executive = 'nvim_lsp'
 
 -- Ensure you have installed some decent font to show these pretty symbols,
 -- then you can enable icon for the kind.
-vim.cmd [[let g:vista#renderer#enable_icon = 1]]
-
--- Change some default icons
--- see: https://github.com/slavfox/Cozette/blob/master/img/charmap.txt
-vim.cmd [[
-  let g:vista#renderer#icons = {
-  \   "function": "\u0192",
-  \   "variable": "\uf00d",
-  \   "prototype": "\uf013",
-  \   "macro": "\uf00b",
-  \ }
-]]
+vim.g['vista#renderer#enable_icon'] = 1
 
