9c157a5584478a72018797bc615e9b417cba26b7
brainfucksec <brainfucksec@protonmail.com>
2022-04-25 08:08:22 +0200

    Update /core/options.lua

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -15,7 +15,7 @@ local opt = vim.opt   -- Set options (global/buffer/windows-scoped)
 opt.mouse = 'a'                       -- Enable mouse support
 opt.clipboard = 'unnamedplus'         -- Copy/paste to system clipboard
 opt.swapfile = false                  -- Don't use swapfile
-opt.completeopt = 'menuone,noselect'  -- Autocomplete options
+opt.completeopt = 'menuone,noinsert,noselect'  -- Autocomplete options
 
 -----------------------------------------------------------
 -- Neovim UI
