068e42a6f63645b0c51986a2f173511a62df5616
brainfucksec <brainfucksec@protonmail.com>
2022-09-10 16:56:33 +0200

    Update nvim/lua/plugins/nvim-lspconfig.lua, see ISSUE #23

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -64,7 +64,7 @@ local on_attach = function(client, bufnr)
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Highlighting references
-  if client.resolved_capabilities.document_highlight then
+  if client.server_capabilities.document_highlight then
     vim.api.nvim_exec([[
       augroup lsp_document_highlight
         autocmd! * <buffer>
