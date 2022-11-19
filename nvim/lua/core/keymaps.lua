ac826247430f2fac56f7460be2107bbbff458795
brainfucksec <brainfucksec@protonmail.com>
2022-11-19 13:42:55 +0100

    Changes on /lua/core/keymaps.lua

--- nvim/lua/core/keymaps.lua
+++ nvim/lua/core/keymaps.lua
@@ -3,7 +3,7 @@
 -----------------------------------------------------------
 
 local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
+  local options = { noremap=true, silent=true }
   if opts then
     options = vim.tbl_extend('force', options, opts)
   end
