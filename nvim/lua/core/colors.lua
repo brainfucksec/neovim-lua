bc140ce55a525e57fbec8527c74aac27fa1528b9
brainfucksec <brainfucksec@protonmail.com>
2022-11-20 05:29:13 +0100

    Update: /core/colors.lua

--- nvim/lua/core/colors.lua
+++ nvim/lua/core/colors.lua
@@ -4,9 +4,9 @@
 
 -- See: https://github.com/brainfucksec/neovim-lua#appearance
 
--- Load nvim color scheme:
--- Change the "require" values with your color scheme
--- Available color schemes: onedark, monokai, rose-pine
+-- Neovim UI color scheme.
+-- Add the selected color scheme in the `require` values below.
+-- Current available color schemes: onedark, monokai, rose-pine.
 local status_ok, color_scheme = pcall(require, 'onedark')
 if not status_ok then
   return
@@ -15,17 +15,27 @@ end
 -- Note: The instruction to load the color scheme may vary.
 -- See the README of the selected color scheme for the instruction
 -- to use.
--- e.g.: require('color_scheme').setup{}, vim.cmd('color_scheme')
-
--- OneDark styles: dark, darker, cool, deep, warm, warmer, light
+-- e.g.: require('color_scheme').setup{}, vim.cmd('color_scheme') ...
 require('onedark').setup {
+  -- styles: dark, darker, cool, deep, warm, warmer, light
   style = 'darker',
   colors = { fg = '#b2bbcc' }, --default: #a0a8b7
 }
 require('onedark').load()
 
--- statusline color schemes:
--- import with: require('colors').colorscheme_name
+--[[
+Statusline color schemes.
+Import the following color schemes in your statusline.lua file
+with: `require('core/colors').colorscheme_name` where the colors scheme name
+is the value of the variables below.
+
+e.g.: `local colors = require('core/colors').onedark_dark
+See: `core/statusline.lua`
+
+The color schemes below are created by following the "palette" file color
+schemes. Color names are adapted to maintain a pattern, original names can be
+different.
+--]]
 local M = {}
 
 -- Theme: OneDark (dark)
