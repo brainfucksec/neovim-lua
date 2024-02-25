bcb845fef58ddb92e01a295bdedf81fc2e05cdbf
David Holt <fugimuse@gmail.com>
2024-02-25 18:46:44 -0500

    Start with everything disabled

--- nvim/init.lua
+++ nvim/init.lua
@@ -7,16 +7,16 @@ Website: https://github.com/brainfucksec/neovim-lua
 --]]
 
 -- Import Lua modules
-require('core/lazy')
-require('core/autocmds')
-require('core/keymaps')
-require('core/colors')
-require('core/statusline')
-require('core/options')
-require('lsp/lspconfig')
-require('plugins/nvim-tree')
-require('plugins/indent-blankline')
-require('plugins/nvim-cmp')
-require('plugins/nvim-treesitter')
-require('plugins/alpha-nvim')
+--require('core/lazy')
+--require('core/autocmds')
+--require('core/keymaps')
+--require('core/colors')
+--require('core/statusline')
+--require('core/options')
+--require('lsp/lspconfig')
+--require('plugins/nvim-tree')
+--require('plugins/indent-blankline')
+--require('plugins/nvim-cmp')
+--require('plugins/nvim-treesitter')
+--require('plugins/alpha-nvim')
 
