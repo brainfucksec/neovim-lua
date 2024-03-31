8e01fe90070caf92470e8bd1c6a5be040009a665
brainfucksec <brainfucksec@protonmail.com>
2024-03-31 10:03:36 +0200

    Added parser names on nvim-treesitter "ensure installed" option

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -5,7 +5,6 @@
 -- Plugin: nvim-treesitter
 -- url: https://github.com/nvim-treesitter/nvim-treesitter
 
-
 local status_ok, nvim_treesitter = pcall(require, 'nvim-treesitter.configs')
 if not status_ok then
   return
@@ -14,9 +13,10 @@ end
 -- See: https://github.com/nvim-treesitter/nvim-treesitter#quickstart
 nvim_treesitter.setup {
   -- A list of parser names, or "all"
+  -- See: `TSInstallInfo` commmand
   ensure_installed = {
-    'bash', 'c', 'cpp', 'css', 'html', 'javascript', 'json', 'lua', 'python',
-    'rust', 'typescript', 'vim', 'yaml',
+    'awk', 'bash', 'c', 'cmake', 'cpp', 'css', 'html', 'javascript', 'json',
+    'lua', 'python', 'rust', 'typescript', 'vim', 'yaml',
   },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
