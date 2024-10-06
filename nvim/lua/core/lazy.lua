571b6ab7d60990539f80a481acad654c531f64fb
brainfucksec <brainfucksec@protonmail.com>
2024-10-06 14:18:59 +0200

    Update nvim/lua/core/lazy.lua (lazy improvement: 1 prep)

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -4,22 +4,28 @@
 
 -- Plugin manager: lazy.nvim
 -- URL: https://github.com/folke/lazy.nvim
+-- https://lazy.folke.io/
 
--- For information about installed plugins see the README:
--- neovim-lua/README.md
--- https://github.com/brainfucksec/neovim-lua#readme
+--[[
+For information about installed plugins see the README:
+neovim-lua/README.md: Plugins
+https://github.com/brainfucksec/neovim-lua?tab=readme-ov-file#plugins
+--]]
 
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+      { out, "WarningMsg" },
+      { "\nPress any key to exit..." },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -44,8 +50,7 @@ lazy.setup({
       priority = 1000, -- make sure to load this before all the other start plugins
     },
 
-    -- other colorschemes:
-
+    -- Other color schemes:
     { 'rebelot/kanagawa.nvim', lazy = true },
     { 'tanvirtin/monokai.nvim', lazy = true },
     { 'https://github.com/rose-pine/neovim', name = 'rose-pine', lazy = true },
