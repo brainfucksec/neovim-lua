6423f67e892d6c7f5ea4649e23330b6227e25e46
brainfucksec <brainfucksec@protonmail.com>
2024-10-06 13:38:22 +0200

    Fix nvim/lua/core/lazy.lua

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -39,7 +39,7 @@ lazy.setup({
     See: /nvim/lua/core/colors.lua
     --]]
     {
-      'navarasu/onedark.nvim'
+      'navarasu/onedark.nvim',
       lazy = false, -- make sure we load this during startup if it is your main colorscheme
       priority = 1000, -- make sure to load this before all the other start plugins
     },
