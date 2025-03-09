613279aa2d7a0352e3a4f9a279d8b4218c51c35c
brainfucksec <brainfucksec@protonmail.com>
2025-03-09 08:54:44 +0100

    Update dashboard (alpha.nvim) config

--- nvim/lua/plugins/alpha-nvim.lua
+++ nvim/lua/plugins/alpha-nvim.lua
@@ -3,10 +3,12 @@
 -----------------------------------------------------------
 
 -- Plugin: alpha-nvim
--- url: https://github.com/goolord/alpha-nvim
+-- URL: https://github.com/goolord/alpha-nvim
 
--- Configuration start: https://github.com/goolord/alpha-nvim#quick-start
--- For configuration examples (alpha-themes), see: https://github.com/goolord/alpha-nvim/discussions/16
+--[[
+Configuration start: https://github.com/goolord/alpha-nvim#quick-start
+Configuration examples (alpha-themes): https://github.com/goolord/alpha-nvim/discussions/16
+--]]
 
 local status_ok, alpha = pcall(require, 'alpha')
 if not status_ok then
@@ -19,9 +21,9 @@ local dashboard = require('alpha.themes.dashboard')
 
 
 -- Load banner:
--- Set the different banners to the variables `bx` (b0, b1, ..)
+-- Set the different banners to the variables `bx` (b0,b1,..),
 -- to add a new banner create a new variable `bx` to the list :)
--- by default there are six different banners (from b0 to b5).
+-- By default there are six different banners (from b0 to b5).
 local b0 = {
   "                                                    ",
   " ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
@@ -105,12 +107,15 @@ dashboard.section.buttons.val = {
   dashboard.button('q', '  Quit', ':qa<CR>'),
 }
 
--- Footer
+-- Load Footer
 local function footer()
+  -- Print nvim package version
   local version = vim.version()
   local print_version = "v" .. version.major .. '.' .. version.minor .. '.' .. version.patch
+  -- Print Datetime
   local datetime = os.date('%Y/%m/%d %H:%M')
 
+  -- Example output: v0.10.4 - 2025/03/09 08:47
   return print_version .. ' - ' .. datetime
 end
 
