b7fc1f74266a4cb552515fe8f5410f4ea6720858
brainfucksec <brainfucksec@protonmail.com>
2025-04-05 20:33:30 +0200

    Fix nvim-treesitter config:
    - Removed redundant `vim` parser.
    
    This parser already exists on `/usr/share/nvim/runtime/parser/vim.so`

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -18,7 +18,7 @@ nvim_treesitter.setup {
     'angular', 'awk', 'bash', 'c', 'c_sharp', 'cmake', 'cpp', 'css', 'csv',
     'go', 'haskell', 'html', 'http', 'java', 'javascript', 'json', 'lua',
     'luadoc', 'make', 'markdown', 'markdown_inline', 'nix', 'perl', 'python',
-    'ruby','rust', 'sql', 'toml', 'typescript', 'vim', 'yaml',
+    'ruby','rust', 'sql', 'toml', 'typescript', 'yaml',
   },
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
